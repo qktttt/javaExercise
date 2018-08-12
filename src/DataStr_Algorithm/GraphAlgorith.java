42f9e2d566f8c734ea946bad302569a9da053da0
Kunting Qi <qik@miamioh.edu>
2018-08-12 07:07:04 -0400

    edit graph algorithm

---
+++ src/DataStr_Algorithm/GraphAlgorith.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class GraphAlgorithm
+{
+	public void bfs(DirectedGraph graph, Vertice origin)
+	{
+		pointer = 0;
+		for(Vertice each : graph.vertices)
+		{
+			vertices[pointer++] = each;
+
+		}
+
+	}
+
+	public void dfs(DirectedGraph graph, Vertice origin)
+	{
+
+	}
+}
+
+class VerticeInAlgo
+{
+	public VerticeInAlgo(DirectedGraph graph)
+	{
+
+	}
+}
